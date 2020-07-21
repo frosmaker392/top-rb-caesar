eb28c9f684b58a6b233a888470e437515fb386ac
zhafir <zzzhafir75@gmail.com>
2020-07-21 17:32:44 +0800

    Add caesar_cipher method
    Add lines for ciphertext output

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -17,3 +17,27 @@ while !valid_n
     puts "n is not an integer! TRY ANOTHER"
   end
 end
+
+def caesar_cipher(plaintext, shiftAmount)
+  normalisedShift = shiftAmount % 26
+  ciphertext = ""
+
+  plaintext.each_byte do |c|
+    uppercase = c > 64 && c < 91
+    lowercase = c > 96 && c < 123
+
+    if uppercase || lowercase
+      c += normalisedShift
+
+      outOfRange = c > 90 && uppercase || c > 122 && lowercase
+      c -= outOfRange ? 26 : 0;
+    end
+
+    ciphertext += c.chr
+  end
+
+  return ciphertext
+end
+
+puts "Ciphertext with shift value #{n} : "
+puts caesar_cipher(input, n)
